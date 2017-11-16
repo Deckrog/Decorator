2717988858cf12d84c57159b541abc1df458cb4f
Deckrog <deckrog@mail.ru>
2017-11-16 10:01:19 +0300

    Delete DivDecorator.java

--- DivDecorator.java
+++
@@ -1,9 +0,0 @@
-public class DivDecorator implements IDecorator{
-    String s;
-    public DivDecorator(String s){
-        this.s=s;
-    }
-    public void print(){
-        System.out.print(s);
-    }
-}
